eeeab8f42419ffdb1f067e8f08f7f6ece620b9b3
aninder <aninder@gmail.com>
2015-02-08 10:05:23 +0530

    added autoloading of controllers

--- lib/rack_mvc.rb
+++ lib/rack_mvc.rb
@@ -1,5 +1,7 @@
 require "rack_mvc/version"
 require "rack_mvc/controller"
+#$LOAD_PATH << File.join(File.dirname(__FILE__),"..","app","controllers")
+$LOAD_PATH << File.join("app","controllers")
 
 module RackMvc
   class Application
@@ -8,8 +10,12 @@ module RackMvc
         return [302, {"location"=>"/pages/check"}, []]
       end
       _,controller_class,action = env["PATH_INFO"].split("/") 
-      response = Object.const_get(controller_class.capitalize+"Controller").new.send(action)
+      response = self.class.const_get(controller_class.capitalize+"Controller").new.send(action)
       [200, {"Content-Type"=>"text/html"}, [response] ]
+    end
+    def self.const_missing(const)
+      require const.to_s.gsub(/^([A-Z][a-z]+)([A-Z][a-z]+)/,'\1_\2').downcase
+      const_get(const)
     end
   end
 end
