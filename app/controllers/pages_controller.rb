f5ed81c5a025d019d8d9d30f4dbc6e17cf73c8a9
aninder <aninder@gmail.com>
2015-02-08 05:33:40 +0530

    moved app to a different project

--- app/controllers/pages_controller.rb
+++
@@ -1,8 +0,0 @@
-#require_relative "../../lib/rack_mvc/controller" 
-
-class PagesController < RackMvc::Controller
-  def check
-    render(:default, con:"pages controller")
-  end
-end
-
--- lib/rack_mvc/controller.rb
+++ lib/rack_mvc/controller.rb
@@ -4,7 +4,7 @@ require 'tilt/erubis'
 module RackMvc
   class Controller
     def render(view_name, variables)
-      template_file = File.join(File.dirname(__FILE__), "..", "..", "app", "views", self.class.to_s.gsub(/Controller$/,"").downcase, "#{view_name}.erb")
+      template_file = File.join("app", "views", self.class.to_s.gsub(/Controller$/,"").downcase, "#{view_name}.erb")
       template = Tilt::ErubisTemplate.new(template_file)          
       template.render(Object.new, variables)
     end
